d9057f3cbb72baf85f489d2530620be6f72c73a7
KevinAlves55 <programador.kevin4773@gmail.com>
2022-12-26 17:03:34 -0300

    feat: Criando uma rota para pegar apenas um registro

--- api/contatos/get.php
+++ api/contatos/get.php
@@ -16,5 +16,17 @@
       echo json_encode(["dados" => "Não existem dados de contatos"]);
       echo header("HTTP/1.1 500 Internal Server Error");
     }
+  } else if ($acao === "listar" && $params !== "") {
+    $db = DB::connect();
+    $rsDados = $db->prepare("SELECT * FROM tblcontatos WHERE id={$params}");
+    $rsDados->execute();
+    $result = $rsDados->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($result) {
+      echo json_encode($result);
+    } else {
+      echo json_encode(["dados" => "Não existem dados de contatos"]);
+      echo header("HTTP/1.1 500 Internal Server Error");
+    }
   }
 ?>

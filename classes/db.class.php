c2eabf28e0fe67e676ad4b6af50dd35e64db24fa
KevinAlves55 <programador.kevin4773@gmail.com>
2022-12-27 02:45:47 -0300

    fix: Informação no arquivo db.class.php

--- classes/db.class.php
+++ classes/db.class.php
@@ -1,6 +1,7 @@
 <?php
   class DB {
     public static function connect() {
+      // Usar as informações do seu banco e local
       $local = "localhost";
       $user = "root";
       $password = "bcd127";
